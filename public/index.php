0e203caca8973fe7cb8d7480106e685b7b9a7627
Corentin L <40762269+Octanne@users.noreply.github.com>
2023-10-04 00:53:16 +0200

    Update index.php to fix includes path to absolute

--- public/index.php
+++ public/index.php
@@ -162,7 +162,7 @@ echo <<<HTML
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="includes/style.css">
+    <link rel="stylesheet" href="/includes/style.css">
   </head>
   <body style="padding-top: 5em; padding-bottom: 5em;" data-bs-theme="dark">
     <div class="container">
